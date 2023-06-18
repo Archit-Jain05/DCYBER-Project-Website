d400c2a82177dd302b38bad92df24d677b132b11
Archit-Jain05 <architjain2005@gmail.com>
2023-06-19 00:07:41 +0530

    1.1

--- src/component/header.js
+++ src/component/header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../styles/Header.css'
+import LogoW from '../assets/LogoW.png'
 
 function header() {
   return (
     <div>
-      <div className="left"><p>Hii</p></div>
-      <div className="right"></div>
+      <div className="left"><p><img src={LogoW} alt='Dcyber Logo' className='left'></img></p></div>
+      <div className="right">hiiii</div>
     </div>
   )
 }
