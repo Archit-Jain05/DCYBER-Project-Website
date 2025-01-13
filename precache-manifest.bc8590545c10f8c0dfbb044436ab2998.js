a7493289616036366ad802ed7395436cdfa51865
Archit Jain <architjain2005@gmail.com>
2025-01-14 00:15:05 +0530

    Updates

---
+++ precache-manifest.bc8590545c10f8c0dfbb044436ab2998.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eace8c2a682f2fca5fe8c95d9cc48092",
+    "url": "/DCYBER-Project-Website/index.html"
+  },
+  {
+    "revision": "debb25cfa6fd74fb52af",
+    "url": "/DCYBER-Project-Website/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "5843b6b5dfb31bfde063",
+    "url": "/DCYBER-Project-Website/static/css/main.5363b873.chunk.css"
+  },
+  {
+    "revision": "debb25cfa6fd74fb52af",
+    "url": "/DCYBER-Project-Website/static/js/2.8f412abf.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/DCYBER-Project-Website/static/js/2.8f412abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c38363d5d61b7f989bb",
+    "url": "/DCYBER-Project-Website/static/js/3.52e95e4d.chunk.js"
+  },
+  {
+    "revision": "5843b6b5dfb31bfde063",
+    "url": "/DCYBER-Project-Website/static/js/main.fa86afe1.chunk.js"
+  },
+  {
+    "revision": "8a43f2ae5bbcae5ae127",
+    "url": "/DCYBER-Project-Website/static/js/runtime-main.e00ea94b.js"
+  },
+  {
+    "revision": "a36a4153471f2536dea507e05820e3c7",
+    "url": "/DCYBER-Project-Website/static/media/Framework.a36a4153.jpeg"
+  },
+  {
+    "revision": "cdfd7058d9a3ec9f2d03c2183d0f1b33",
+    "url": "/DCYBER-Project-Website/static/media/LogoW.cdfd7058.png"
+  },
+  {
+    "revision": "06b6f6480533a644b7e5cf89d0a8904d",
+    "url": "/DCYBER-Project-Website/static/media/LogoWC.06b6f648.png"
+  },
+  {
+    "revision": "97ac5b7273116b0af8e56365f9b3b345",
+    "url": "/DCYBER-Project-Website/static/media/Partner1.97ac5b72.jpg"
+  },
+  {
+    "revision": "adbd2ec9a34772292053cbd0af37461d",
+    "url": "/DCYBER-Project-Website/static/media/bgv2.adbd2ec9.mp4"
+  },
+  {
+    "revision": "4a3dd13724397eaaf51b7b792ff20691",
+    "url": "/DCYBER-Project-Website/static/media/blog1.4a3dd137.png"
+  },
+  {
+    "revision": "a28433705c56e82d0212a8161ef0e2da",
+    "url": "/DCYBER-Project-Website/static/media/blog2.a2843370.jpg"
+  },
+  {
+    "revision": "eca6f5eda3f2196ee88f0a824bfb0f58",
+    "url": "/DCYBER-Project-Website/static/media/blog3.eca6f5ed.jpg"
+  },
+  {
+    "revision": "b45867b0f0e8fcf16ffbd3fc65f85822",
+    "url": "/DCYBER-Project-Website/static/media/def.b45867b0.png"
+  },
+  {
+    "revision": "017ceef8267b76e7a73afce03c6bc1c8",
+    "url": "/DCYBER-Project-Website/static/media/fms.017ceef8.png"
+  },
+  {
+    "revision": "8d514b3ee156bd401494a0dec3cabb5c",
+    "url": "/DCYBER-Project-Website/static/media/p10.8d514b3e.png"
+  },
+  {
+    "revision": "9eaa35a411ccb1d9e729d36e2aea6a0b",
+    "url": "/DCYBER-Project-Website/static/media/p13.9eaa35a4.png"
+  },
+  {
+    "revision": "16f9a427d67fa4d081738ca3bc3d6481",
+    "url": "/DCYBER-Project-Website/static/media/p14.16f9a427.png"
+  },
+  {
+    "revision": "7fcb04a581ab74ba07caa51523fba86a",
+    "url": "/DCYBER-Project-Website/static/media/p15.7fcb04a5.png"
+  },
+  {
+    "revision": "67125e2d5d5313a92f472673c7850ca3",
+    "url": "/DCYBER-Project-Website/static/media/p16.67125e2d.jpg"
+  },
+  {
+    "revision": "47228faffd67c352e413e4073b153b72",
+    "url": "/DCYBER-Project-Website/static/media/p2.47228faf.png"
+  },
+  {
+    "revision": "367431258ffb3a070bcc6685714814c6",
+    "url": "/DCYBER-Project-Website/static/media/p3.36743125.png"
+  },
+  {
+    "revision": "7485cac867206521863672715652a00e",
+    "url": "/DCYBER-Project-Website/static/media/p5.7485cac8.png"
+  },
+  {
+    "revision": "a7bafca0f03eab8e4ed6788bf425a690",
+    "url": "/DCYBER-Project-Website/static/media/p6.a7bafca0.png"
+  }
+]);
